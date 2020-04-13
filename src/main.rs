2ebd0c5bfd9ff15493501128decc42ee6df95f38
Tony Head <tony.head@dws.la>
2020-04-13 09:03:31 -0700

    Refactor f registers to use FlagRegisters and implement ADD instruction

--- src/main.rs
+++ src/main.rs
@@ -56,16 +56,17 @@ impl Registers
     fn get_af(&self) -> u16
     {
         (self.a as u16) << 8
-        | self.f as u16
+        | u8::from(self.f) as u16
     }
 
     fn set_af(&mut self, value: u16)
     {
         self.a = ((value & 0xFF00) >> 8) as u8;
-        self.f = (value & 0xFF) as u8;
+        self.f = FlagsRegister::from((value & 0xFF) as u8);
     }
 }
 
+#[derive(Copy, Clone)]
 struct FlagsRegister
 {
     zero: bool,
@@ -105,7 +106,59 @@ impl std::convert::From<u8> for FlagsRegister
 
 }
 
+enum Instruction
+{
+    ADD(ArithmeticTarget),
+}
+ enum ArithmeticTarget
+{
+    A, B, C, D, E, H, L,
+}
+
+struct CPU {
+    registers: Registers,
+}
+
+impl CPU
+{
+    fn execute(&mut self, instruction: Instruction)
+    {
+        match instruction
+        {
+            Instruction::ADD(target) =>
+            {
+                match target
+                {
+                    ArithmeticTarget::C =>
+                    {
+                        let value = self.registers.c;
+                        let new_value = self.add(value);
+                        self.registers.a = new_value;
+                    }
+                    _=> { /* TODO: support more targets */ }
+                }
+            }
+            _=> { /* TODO: support more targets */ }
+        }
+    }
+
+    fn add(&mut self, value: u8) -> u8
+    {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+
+        // set flags
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = did_overflow;
+        // Half Carry is set if adding the lower nibbles of the value and register A
+        // together result in a value bigger than 0xF. If the result is larger than 0xF
+        // then the addition caused a carry from the lower nibble to the upper nibble.
+        self.registers.f.half_carry = (self.registers.a &0xF) + (value & 0xF) > 0xF;
+        new_value
+    }
+}
+
 fn main()
 {
-    println!("Hello, world!");
+    println!("Hello, Gameboy!");
 }
