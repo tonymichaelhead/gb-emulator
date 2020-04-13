c3823aedab427b0a70c0047be7838835183aacd9
Tony Head <tony.head@dws.la>
2020-04-12 23:24:38 -0700

    Add FlagsRegister struct and conversion methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
 struct Registers
 {
     a: u8,
@@ -5,7 +10,7 @@ struct Registers
     c: u8,
     d: u8,
     e: u8,
-    f: u8,
+    f: FlagsRegister,
     h: u8,
     l: u8,
 }
@@ -67,6 +72,37 @@ struct FlagsRegister
     subtract: bool,
     half_carry: bool,
     carry: bool,
+}
+
+impl std::convert::From<FlagsRegister> for u8
+{
+    fn from(flag: FlagsRegister) -> u8
+    {
+        (if flag.zero       { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION |
+        (if flag.subtract   { 1 } else { 0 }) << SUBRACT_FLAG_BYTE_POSITION |
+        (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION |
+        (if flag.carry      { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
+impl std::convert::From<u8> for FlagsRegister
+{
+    fn from(byte: u8) -> Self
+    {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+
+        FlagsRegister
+        {
+            zero,
+            subtract,
+            half_carry,
+            carry,
+        }
+    }
+
 }
 
 fn main()
